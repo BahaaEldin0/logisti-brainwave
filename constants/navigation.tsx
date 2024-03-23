f78493a15563808b5875403875941a51fe27d5d7
BahaaEldin0 <b.moustafa@nu.edu.eg>
2024-03-23 08:03:24 +0200

    Start

---
+++ constants/navigation.tsx
@@ -0,0 +1,32 @@
+export const navigation = [
+    {
+        title: "Photo editing",
+        icon: "image-check",
+        color: "#8E55EA",
+        url: "/photo-editing",
+    },
+    {
+        title: "Video generation",
+        icon: "play-circle",
+        color: "#D84C10",
+        url: "/video-generation",
+    },
+    {
+        title: "Education feedback",
+        icon: "trophy",
+        color: "#0084FF",
+        url: "/education-feedback",
+    },
+    {
+        title: "Code generation",
+        icon: "codepen",
+        color: "#52BA69",
+        url: "/code-generation",
+    },
+    {
+        title: "Audio generation",
+        icon: "music-note",
+        color: "#E68A1D",
+        url: "/audio-generation",
+    },
+];
